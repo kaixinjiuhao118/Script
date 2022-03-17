0f3b63e8661c35e43fc2696c9d29cf2fcb1490ae
kaixinjiuhao118 <71906904+kaixinjiuhao118@users.noreply.github.com>
2022-03-18 01:01:39 +0800

    Update dyjs_cookies.js

--- dyjs_cookies.js
+++ dyjs_cookies.js
@@ -2,8 +2,8 @@
 let cookies = [
     {
         "name": "账号1",
-        "Cookie": "xxxxxxxxx",
-        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
+        "Cookie": "excgd=20220314; passport_csrf_token=619da46aa2b2340f89938a87de80f4b7; passport_csrf_token_default=619da46aa2b2340f89938a87de80f4b7; d_ticket=7a7b0ce8a943aa1c432cc56ae27527b71bc15; n_mh=XRO_GBJeFxE-po1Q1ofi_chdPLaLXG5ZnXyabU8JLfo; odin_tt=62b02a66c7113564c0f615797194718286ff6025ba04bc3da416c82918dba389379b425780213dbc14349a3a5a293b581eb7b27b4b63bceaf88787d49351ed7b; sessionid=7def03a0c6e80f2e0e22869efcf9c856; sessionid_ss=7def03a0c6e80f2e0e22869efcf9c856; sid_guard=7def03a0c6e80f2e0e22869efcf9c856%7C1647219253%7C5184000%7CFri%2C+13-May-2022+00%3A54%3A13+GMT; sid_tt=7def03a0c6e80f2e0e22869efcf9c856; uid_tt=69e281c29c325b73786afcd74ae3cfed; uid_tt_ss=69e281c29c325b73786afcd74ae3cfed; msToken=MqFRx6gDfaKA0vrMoVFJlL5vUQXBDAyPeDd7J-1IL2WP93fVioyTihlz1vGQmCAawza74yTLGbIu9TrfMdKx8Vll; MONITOR_WEB_ID=56162634115; install_id=2115886165528587; ttreq=1$392339741c2b175ca78189aa4e5a5d002a2f1805\",
+        "x-Tt-Token": "007def03a0c6e80f2e0e22869efcf9c85606121093675b16f4eade874b991039cc6e1c18a9894ea971a38abbe59f09baed688976536e9d0866ae708bb6ae777cf6307c31d62dac6a9865d763c4042f84b6784d853307244db344cce6edbeefd5851cf-1.0.1\"
     },
     {
         "name": "账号2",
